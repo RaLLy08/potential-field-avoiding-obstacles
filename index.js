b1aa2e2a19360611b7c6f05fe416774d1e6f9456
azad.k <uytemataaero@mail.ru>
2022-05-13 01:21:39 +0400

    preact

--- index.js
+++ index.js
@@ -1,65 +1,118 @@
 import { CanvasRenderer } from './Canvas.js';
 import { Obstacles, Vehicle, Target } from './Entity.js';
-import { canvasActions } from './view/Components.js';
+import html, { Component, render } from './preact/index.js';
+import { CanvasComponent } from './view/Components.js';
+import ParamsPanel from './view/ParamsPanel.js';
+import { withLsSubscribe } from './view/helpers.js';
+
+// mutatable params 
+const canvasParamsStates = withLsSubscribe({
+  vectorFlow: 0,
+  totalForce:0,
+  vectorsFlow:0,
+  attractiveForce:0,
+  repulsiveForceTotal:0,
+  repulsiveForceNewTotal:0,
+  repulsiveForce:0,
+  repulsiveForceNew:0,
+  obstaclesFieldRadius:0,
+  vehicle: 1,
+  vehicleFieldRadius:0,
+  obstacles: 1,
+  obstaclesMap: 0,
+});
+
+
 
 const obstacles = new Obstacles(
-    Obstacles.DEFAULT
+    Obstacles.MAPS[canvasParamsStates.obstaclesMap]
 );
-
 const target = new Target(1000, CanvasRenderer.HEIGHT/2, 2.5, 0.00015);
 const vehicle = new Vehicle(100, CanvasRenderer.HEIGHT/2, 150)
 
 
-const canvasRenderer = new CanvasRenderer(vehicle, target, obstacles);
 
+const canvasParamsActions = {
+  obstaclesMap: (value) => obstacles.set(Obstacles.MAPS[value])
+}
 
-let pause = false;
+const paramsPanel = {
+  vehicle,
+  canvasParamsStates,
+  canvasParamsActions
+}
 
-const frame = () => {
-    canvasRenderer.frame();
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    const canvas = document.getElementById('canvas');
+    
+    this.canvasRenderer = new CanvasRenderer(canvas, canvasParamsStates, vehicle, target, obstacles);
+    
 
+    this.canvasRenderer.element.onmousedown = (e) => {
+      pressed = true;
+      // obstacles.add(new Obstacle(e.offsetX, e.offsetY, 100, 2, 0.00015, 18))
+    }
+  
+    this.canvasRenderer.element.onmousemove = (e) => {
+        if (!pressed) return;
+        target.x = e.offsetX;
+        target.y = e.offsetY;
+    }
+    
+    this.canvasRenderer.element.onmouseup = (e) => {
+        pressed = false;
+    }
+
+    this.frame();
+  }
+
+  frame = () => {
+    this.canvasRenderer.frame();
+    this.forceUpdate();
     if (!pause) {
         vehicle.setAtractiveForce(target);
         vehicle.setObstacles(obstacles.getObstaclesInVehicleRadius(vehicle))
-
         vehicle.move();
     };
- 
-    window.requestAnimationFrame(frame)
+
+    window.requestAnimationFrame(this.frame)
+  }
+
+
+  render() {
+    return html`
+        ${ParamsPanel(paramsPanel)}
+        ${CanvasComponent({
+            id: 'canvas',
+            onPauseResume,
+            onResetVehicle,
+            isPaused: pause,
+        })}
+    `;
+  }
 }
-// **TODO:
-// when radius on color near objects
-
-frame();
 
 
-canvasActions.onResetVehicle = () => {
+render(html`<${App} />`, document.body);
+
+
+var pause = false;
+var pressed = false;
+
+function onResetVehicle() {
     vehicle.x = 100;
     vehicle.y = CanvasRenderer.HEIGHT/2;
 }
 
-canvasActions.onResume = () => {
+function onPauseResume() {
+  if (pause) {
     pause = false;
-}
-canvasActions.onPause = () => { 
-    pause = true;
-}
-
-
-// // skip it, only for testing
-let pressed = false;
-
-canvasRenderer.element.onmousedown = (e) => {
-    pressed = true;
-    // obstacles.add(new Obstacle(e.offsetX, e.offsetY, 100, 2, 0.00015, 18))
+    return;
+  }
+  pause = true;
 }
 
-canvasRenderer.element.onmousemove = (e) => {
-    if (!pressed) return;
-    target.x = e.offsetX;
-    target.y = e.offsetY;
-}
-
-canvasRenderer.element.onmouseup = (e) => {
-    pressed = false;
-}
--- view/Components.js
+++ view/Components.js
@@ -1,281 +1,171 @@
-import { COLOR } from "../consts.js";
-import { initCheckbox, onCheckboxChange, onSelectChange, withLsSubscribe } from "./helpers.js";
+import html from "../preact/index.js"
 
-class VehicleDisplay {
-    static TOTAL_FORCE_ID = "total-force";
-    static ATTRACTIVE_FORCE_ID = "attractive-force";
-    static REPULSIVE_FORCE_TOTAL_ID = "repulsive-force-total";
-    static REPULSIVE_FORCE_NEW_TOTAL_ID = "repulsive-force-new-total";
-    static THETA_ID = "theta";
-    static SIGMA_ID = "sigma";
-    static GAMMA_ID = "gamma";
-    static X_ID = "x";
-    static Y_ID = "y";
+export function CheckboxWithColor({
+    id,
+    text,
+    title,
+    checked,
+    color,
+    onChange,
+}) {
+    const style = {
+        wrapper: {
+            display: 'flex',
+            alignItems: 'center',
+        },
+        label: {
+            display: 'flex',
+            alignItems: 'center',
+            cursor: 'pointer',
+        },
+        colorBox: {
+            width: '1em',
+            height: '1em',
+            marginRight: '0.4rem',
+            backgroundColor: color,
+        }
+    }
 
-    #totalForceInput;
-    #attractiveForceInput;
-    #repulsiveForceTotalInput;
-    #repulsiveForceNewTotalInput;
-    #sigmaInput;
-    #thetaInput;
-    #gammaInput;
-    #xInput;
-    #yInput;
-    constructor() {
-        this.#totalForceInput = document.getElementById(
-            VehicleDisplay.TOTAL_FORCE_ID
-        );
-        this.#attractiveForceInput = document.getElementById(
-            VehicleDisplay.ATTRACTIVE_FORCE_ID
-        );
-        this.#repulsiveForceTotalInput = document.getElementById(
-            VehicleDisplay.REPULSIVE_FORCE_TOTAL_ID
-        );
-        this.#repulsiveForceNewTotalInput = document.getElementById(
-            VehicleDisplay.REPULSIVE_FORCE_NEW_TOTAL_ID
-        );
-        this.#sigmaInput = document.getElementById(VehicleDisplay.SIGMA_ID);
-        this.#thetaInput = document.getElementById(VehicleDisplay.THETA_ID);
-        this.#gammaInput = document.getElementById(VehicleDisplay.GAMMA_ID);
-        this.#xInput = document.getElementById(VehicleDisplay.X_ID);
-        this.#yInput = document.getElementById(VehicleDisplay.Y_ID);
+    const handleChange = (e) => {
+        onChange(+e.target.checked);
     }
-    /**
-     * @param {number} value
-     */
-    set totalForce(value) {
-        this.#totalForceInput.value = value.toPrecision(3);
-    }
-    /**
-     * @param {number} value
-     */
-    set attractiveForce(value) {
-        this.#attractiveForceInput.value = value.toPrecision(3);
-    }
-    /**
-     * @param {number} value
-     */
-    set repulsiveForceTotal(value) {
-        this.#repulsiveForceTotalInput.value = value.toPrecision(3);
-    }
-    /**
-     * @param {number} value
-     */
-    set repulsiveForceNewTotal(value) {
-        this.#repulsiveForceNewTotalInput.value = value.toPrecision(3);
-    }
-    /**
-     * F(att) and F(tot) angle
-     * @param {number} value
-     */
-    set theta(value) {
-        this.#thetaInput.value = value.toPrecision(3) + "°";
-    }
-    /**
-     * F(rep) and F(tot) angle
-     * @param {number} value
-     */
-    set sigma(value) {
-        this.#sigmaInput.value = value.toPrecision(3) + "°";
-    }
-    /**
-     * F(att) and F(rep) angle
-     * @param {number} value
-     */
-    set gamma(value) {
-        this.#gammaInput.value = value.toPrecision(3) + "°";
-    }
-    /**
-     * @param {number} value
-     */
-    set x(value) {
-        this.#xInput.value = value.toPrecision(3);
-    }
-    /**
-     * @param {number} value
-     */
-    set y(value) {
-        this.#yInput.value = value.toPrecision(3);
-    }
+
+    return html`
+        <div style=${style.wrapper}>
+            <input checked=${checked} onchange=${handleChange} id=${id} type="checkbox"></input>
+            <label style=${style.label} for=${id} title=${title}>
+                <div style=${style.colorBox}>
+                </div>
+                ${text}
+            </label>
+        </div>
+    `
 }
 
-export const vehicleDisplay = new VehicleDisplay();
-
-class CanvasDisplay {
-    static VECTORS_FLOW_CHECKBOX_ID = "vectors-flow-field-checkbox";
-    static TOTAL_FORCE_CHECKBOX_ID = "total-force-field-checkbox";
-    static ATTRACTIVE_FORCE_CHECKBOX_ID = "attractive-force-field-checkbox";
-    static REPULSIVE_FORCE_TOTAL_CHECKBOX_ID =
-        "repulsive-force-total-field-checkbox";
-    static REPULSIVE_FORCE_NEW_TOTAL_CHECKBOX_ID =
-        "repulsive-force-new-total-field-checkbox";
-    static REPULSIVE_FORCE_CHECKBOX_ID = "repulsive-force-field-checkbox";
-    static REPULSIVE_FORCE_NEW_CHECKBOX_ID =
-        "repulsive-force-new-field-checkbox";
-    static OBSTACLES_FIELD_RADIUS_CHECKBOX_ID =
-        "obstacles-radius-field-checkbox";
-    static OBSTACLES_CHECKBOX_ID = "obstacles-checkbox";
-    static VEHICLE_CHECKBOX_ID = "vehicle-checkbox";
-    static VEHICLE_FIELD_RADIUS_CHECKBOX_ID = "vehicle-field-radius-checkbox";
-    static REPULSIVE_FORCE_COLOR_ID = "repulsive-force-field-color";
-    static REPULSIVE_FORCE_NEW_COLOR_ID = "repulsive-force-new-field-color";
-    static VEHICLE_FIELD_RADIUS_COLOR_ID = "vehicle-field-radius-color";
-    static VEHICLE_COLOR_ID = "vehicle-color";
-    static TOTAL_FORCE_COLOR_ID = "total-force-color";
-    static ATTRACTIVE_FORCE_COLOR_ID = "attractive-force-color";
-    static REPULSIVE_FORCE_TOTAL_COLOR_ID = "repulsive-force-total-color";
-    static REPULSIVE_FORCE_NEW_TOTAL_COLOR_ID =
-        "repulsive-force-new-total-color";
-    static VECTORS_FLOW_COLOR_ID = "vectors-flow-color";
-    static OBSTACLES_FIELD_RADIUS_COLOR_ID = "obstacles-field-radius-color";
-    static OBSTACLES_COLOR_ID = "obstacles-color";
-
-    static OBSTACLES_MAP_SELECT_ID = "obstacles-map-select";
-    static OBSTACLES_MAP_SELECT_OPTIONS = ["default", "wall"];
-    // true/false
-    totalForce = 0;
-    vectorsFlow = 0;
-    attractiveForce = 0;
-    repulsiveForceTotal = 0;
-    repulsiveForceNewTotal = 0;
-    repulsiveForce = 0;
-    repulsiveForceNew = 0;
-    obstaclesFieldRadius = 0;
-    vehicle = 1;
-    vehicleFieldRadius = 0;
-    obstacles = 1;
-
-    obstaclesMap = 0;
-    constructor() {
-        this.#setColors();
+export function InputDisplay({ value, degree }) {
+    const style = {
+        input: {
+            maxWidth: '40px'
+        }
+    }
+    if (degree) {
+        value += '°'
     }
 
-    #setColors() {
-        document.getElementById(
-            CanvasDisplay.TOTAL_FORCE_COLOR_ID
-        ).style.backgroundColor = COLOR.TOTAL_FORCE;
-        document.getElementById(
-            CanvasDisplay.ATTRACTIVE_FORCE_COLOR_ID
-        ).style.backgroundColor = COLOR.ATTRACTIVE_FORCE;
-        document.getElementById(
-            CanvasDisplay.VECTORS_FLOW_COLOR_ID
-        ).style.backgroundColor = COLOR.VECTORS_FLOW;
-        document.getElementById(
-            CanvasDisplay.OBSTACLES_FIELD_RADIUS_COLOR_ID
-        ).style.backgroundColor = COLOR.OBSTACLES_FIELD_RADIUS;
-        document.getElementById(
-            CanvasDisplay.VEHICLE_COLOR_ID
-        ).style.backgroundColor = COLOR.VEHICLE;
-        document.getElementById(
-            CanvasDisplay.VEHICLE_FIELD_RADIUS_COLOR_ID
-        ).style.backgroundColor = COLOR.VEHICLE_FIELD_RADIUS;
-        document.getElementById(
-            CanvasDisplay.REPULSIVE_FORCE_COLOR_ID
-        ).style.backgroundColor = COLOR.REPULSIVE_FORCE;
-        document.getElementById(
-            CanvasDisplay.REPULSIVE_FORCE_NEW_COLOR_ID
-        ).style.backgroundColor = COLOR.REPULSIVE_FORCE_NEW;
-        document.getElementById(
-            CanvasDisplay.REPULSIVE_FORCE_TOTAL_COLOR_ID
-        ).style.backgroundColor = COLOR.REPULSIVE_FORCE;
-        document.getElementById(
-            CanvasDisplay.REPULSIVE_FORCE_NEW_TOTAL_COLOR_ID
-        ).style.backgroundColor = COLOR.REPULSIVE_FORCE_NEW;
-        document.getElementById(
-            CanvasDisplay.OBSTACLES_COLOR_ID
-        ).style.backgroundColor = COLOR.OBSTACLE;
-    }
-}
-const withLs = withLsSubscribe(new CanvasDisplay());
-
-withLs.onSelectChange = () => {};
-
-const valueToElementCheckbox = {
-    totalForce: document.getElementById(CanvasDisplay.TOTAL_FORCE_CHECKBOX_ID),
-    vectorsFlow: document.getElementById(
-        CanvasDisplay.VECTORS_FLOW_CHECKBOX_ID
-    ),
-    attractiveForce: document.getElementById(
-        CanvasDisplay.ATTRACTIVE_FORCE_CHECKBOX_ID
-    ),
-    repulsiveForceTotal: document.getElementById(
-        CanvasDisplay.REPULSIVE_FORCE_TOTAL_CHECKBOX_ID
-    ),
-    repulsiveForceNewTotal: document.getElementById(
-        CanvasDisplay.REPULSIVE_FORCE_NEW_TOTAL_CHECKBOX_ID
-    ),
-    obstaclesFieldRadius: document.getElementById(
-        CanvasDisplay.OBSTACLES_FIELD_RADIUS_CHECKBOX_ID
-    ),
-    vehicle: document.getElementById(CanvasDisplay.VEHICLE_CHECKBOX_ID),
-    vehicleFieldRadius: document.getElementById(
-        CanvasDisplay.VEHICLE_FIELD_RADIUS_CHECKBOX_ID
-    ),
-    repulsiveForce: document.getElementById(
-        CanvasDisplay.REPULSIVE_FORCE_CHECKBOX_ID
-    ),
-    repulsiveForceNew: document.getElementById(
-        CanvasDisplay.REPULSIVE_FORCE_NEW_CHECKBOX_ID
-    ),
-    obstacles: document.getElementById(CanvasDisplay.OBSTACLES_CHECKBOX_ID),
-};
-
-const valueToElementSelect = {
-    obstaclesMap: document.getElementById(
-        CanvasDisplay.OBSTACLES_MAP_SELECT_ID
-    ),
-};
-
-for (const value of Object.keys(valueToElementCheckbox)) {
-    initCheckbox(valueToElementCheckbox[value], withLs[value]);
-    onCheckboxChange(
-        valueToElementCheckbox[value],
-        (state) => (withLs[value] = state)
-    );
+    return html`
+        <input style=${style.input} value="${value}" readonly/>
+    `
 }
 
-for (const value of Object.keys(valueToElementSelect)) {
-    onSelectChange(valueToElementSelect[value], (state) => {
-        const index = CanvasDisplay.OBSTACLES_MAP_SELECT_OPTIONS.indexOf(state);
-
-        withLs.onSelectChange(
-            index,
-            CanvasDisplay.OBSTACLES_MAP_SELECT_OPTIONS
-        );
-    });
-}
-
-export const canvasDisplay = withLs;
-
-class CanvasActions {
-    static RESET_VEHICLE_BUTTON_ID = "reset-vehicle";
-    static PAUSE_RESUME_BUTTON_ID = "pause-resume";
-    onResetVehicle = () => {};
-    onResume = () => {};
-    onPause = () => {};
-
-    constructor() {
-        this.#onButtonClick(
-            document.getElementById(CanvasActions.RESET_VEHICLE_BUTTON_ID),
-            () => this.onResetVehicle()
-        );
-        this.#onButtonClick(
-            document.getElementById(CanvasActions.PAUSE_RESUME_BUTTON_ID),
-            (target) => {
-                if (target.innerText === "Pause") {
-                    this.onPause();
-                    target.innerText = "Resume";
-                    return;
-                }
-                target.innerText = "Pause";
-                this.onResume();
-            }
-        );
+export function Title({ title, text }) {
+    const style = {
+        span: {
+            cursor: 'default',
+            marginRight: '0.2rem',
+            marginLeft: '0.2rem',
+        },
     }
 
-    #onButtonClick = (el, listener) => {
-        el.onclick = (e) => void listener(e.target);
-    };
+    return html`
+        <span style=${style.span} title="${title}">${text}</span>
+    `
 }
 
-export const canvasActions = new CanvasActions();
+export function Details({ summary, children }) { 
+    const style = {
+        details: {
+            marginLeft: "0.8em",
+        },
+        summary: {
+            fontSize: '1.2em',
+            cursor: "pointer",
+        },
+    }
+
+    return html`
+        <details style=${style.details} open>
+            <summary style=${style.summary}>${summary}</summary>
+            ${children}
+        </details>`
+}
+
+export function Table({
+    tableBody,
+}) {
+    const style = {
+        table: {
+            marginTop: '0.4em',
+            marginBottom: '0.4em',
+        }
+    }
+
+    return html`
+        <table style=${style.table}>
+            <tbody>
+                ${tableBody.map(rows => {
+                    return html`<tr>
+                        ${rows.map(cell => {
+                            return html`<td>${cell}</td>`
+                        })}
+                    </tr>`
+                })}
+            </tbody>
+        </table>`
+}
+
+export function CanvasComponent({
+    id,
+    onPauseResume=()=>{},
+    onResetVehicle=()=>{},
+    isPaused,
+}) {
+    const pauseResume = isPaused ? 'Pause' : 'Resume';
+
+    const style = {
+        canvasWrapper: {
+            transform: 'translate(-50%, -50%)', 
+            position: 'absolute', 
+            top: '50%', 
+            left: '50%', 
+            display: 'flex',
+        },
+        canvas: { 
+            border: 'dashed 1px',
+        },
+        resetVehicle: {
+            cursor: 'pointer',
+            position: 'absolute',
+            bottom: '-40px',
+            right: 0,
+        },
+        pauseResume: {
+            cursor: 'pointer',
+            position: 'absolute',
+            bottom: '-40px',
+            right: '110px',
+        }
+    }
+
+    return html`
+        <div class="canvasWrapper" style=${style.canvasWrapper}>
+            <canvas id=${id} style=${style.canvas}></canvas>
+            <button onclick=${onResetVehicle} style=${style.resetVehicle}>Reset vehicle</button>
+            <button onclick=${onPauseResume} style=${style.pauseResume}>${pauseResume}</button>
+        </div>
+    `
+}
+
+export function Select({
+    options=[],
+    onChange,
+    defaultValue
+}) {
+    const handleChange = (e) => onChange(e.target.value);
+    return html`
+        <select value=${defaultValue} onchange=${handleChange}>
+            ${options.map((el, i) => {
+                return html`<option value=${i}>${el}</option>`
+            })}
+        </select>  
+    `
+}
